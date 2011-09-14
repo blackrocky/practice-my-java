cf768846e9a23d783fc31c0aacd625ec842e2fc8
robbie.martinus@gmail.com <robbie.martinus@gmail.com@7c5fe160-6610-cd85-b8be-3f62a134fe3b>
2011-09-14 21:13:05 +0000

    Some basic java stuff

---
+++ trunk/practice-my-java/src/com/googlecode/practicemyjava/basic/AlternativeMainMethod.java
@@ -0,0 +1,18 @@
+package com.googlecode.practicemyjava.basic;
+
+/**
+ * Alternative main method declaration
+ * 
+ * @author blackrocky
+ *
+ */
+public class AlternativeMainMethod {
+/*	public static void main(String[] args) {
+		System.out.println("Heya123");
+	}
+
+*/	public static void main(String... args) {
+		System.out.println("Heya456");
+	}
+
+}
