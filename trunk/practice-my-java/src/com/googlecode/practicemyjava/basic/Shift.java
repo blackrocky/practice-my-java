55d5c4fce913763be22945626d0a62ef0b2ccc38
robbie.martinus@gmail.com <robbie.martinus@gmail.com@7c5fe160-6610-cd85-b8be-3f62a134fe3b>
2011-09-14 21:14:13 +0000

    bit shifter

---
+++ trunk/practice-my-java/src/com/googlecode/practicemyjava/basic/Shift.java
@@ -0,0 +1,36 @@
+package com.googlecode.practicemyjava.basic;
+
+/**
+ * Bit shifter
+ * 
+ * @author blackrocky
+ *
+ */
+public class Shift {
+	public static void main(String[] args) {
+		int x = -1;
+		x = x >>> 5;
+		
+		System.out.println("x = " + x);
+		
+		int b = -1;
+		b = b >> 5;
+		
+		System.out.println("b = " + b);
+		
+		byte y = -1;
+		y = (byte) ( y >>> 5 );
+		
+		System.out.println("y = " + y);
+		
+		int z = -1;
+		z = z << 16;
+		
+		System.out.println("z = " + z);
+		 
+		int a = -1;
+		a = a >>> 32;
+		
+		System.out.println("a = " + a);
+	}
+}
