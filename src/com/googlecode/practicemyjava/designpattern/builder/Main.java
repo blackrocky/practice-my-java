941e41a52197c2ee044c5ed167d62c44d6fc7510
blackrocky <robbie.martinus@yahoo.com.au>
2015-06-24 06:38:44 +1000

    refactor to using maven

--- src/com/googlecode/practicemyjava/designpattern/builder/Main.java
+++
@@ -1,8 +0,0 @@
-package com.googlecode.practicemyjava.designpattern.builder;
-
-public class Main {
-    public static void main(String[] args) {
-        NutritionFacts nutritionFacts = new NutritionFacts.Builder(100, 10).calories(200).sodium(50).build();
-        System.out.println(nutritionFacts);
-    }
-}
