8e62b2f9033b6daf409e54b244a4fc412a1932d9
robbiemartinus <robbie.martinus@yahoo.com.au>
2016-02-03 11:50:15 +1100

    Added java stream Reduce

--- src/test/java/com/googlecode/practicemyjava/java8/stream/ReduceTest.java
+++ src/test/java/com/googlecode/practicemyjava/java8/stream/ReduceTest.java
@@ -11,15 +11,15 @@ import static org.junit.Assert.assertThat;
 public class ReduceTest {
     @Test
     public void should_find_employee_id_1() throws Exception {
-        final Optional<Employee> employee1 = findOnlyEmployeeWithId("1");
-        assertThat(employee1.isPresent(), is(true));
-        assertThat(employee1.get().getFirstName(), is("Tony"));
+        final Optional<Employee> employee = findOnlyEmployeeWithId("1");
+        assertThat(employee.isPresent(), is(true));
+        assertThat(employee.get().getFirstName(), is("Tony"));
     }
 
     @Test
     public void should_not_find_employee_id_5() throws Exception {
-        final Optional<Employee> employee1 = findOnlyEmployeeWithId("5");
-        assertThat(employee1.isPresent(), is(false));
+        final Optional<Employee> employee = findOnlyEmployeeWithId("5");
+        assertThat(employee.isPresent(), is(false));
     }
 
     @Test(expected = DuplicateException.class)
