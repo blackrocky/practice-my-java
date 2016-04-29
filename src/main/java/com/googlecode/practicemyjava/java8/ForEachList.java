ad6e89a0e65246ed00db3c1af06528629ae229b0
robbiemartinus <robbie.martinus@yahoo.com.au>
2016-04-29 14:24:01 +1000

    Added foreachlist

---
+++ src/main/java/com/googlecode/practicemyjava/java8/ForEachList.java
@@ -0,0 +1,35 @@
+package com.googlecode.practicemyjava.java8;
+
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachList {
+    private static final Logger logger = LoggerFactory.getLogger(ForEachList.class);
+
+    public static void main(String[] args) {
+        List<String> japaneseCities = Lists.newArrayList("Fukuoka", "Hiroshima", "Kanazawa", "Takayama", "Nagoya", "Tokyo");
+
+        logger.info("Traversing using forEach and anonymous class");
+        japaneseCities.forEach(new Consumer<String>() {
+            public void accept(String t) {
+                logger.info("{}", t);
+            }
+        });
+        logger.info("");
+
+        logger.info("Traversing using Consumer implementation");
+        MyConsumer action = new MyConsumer();
+        japaneseCities.forEach(action);
+    }
+}
+
+class MyConsumer implements Consumer<String> {
+    private static final Logger logger = LoggerFactory.getLogger(MyConsumer.class);
+    public void accept(String t) {
+        logger.info("{}", t);
+    }
+}
