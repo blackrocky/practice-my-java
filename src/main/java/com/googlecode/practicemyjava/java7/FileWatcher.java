e619df40c0cd8583c57eaab2d7e2fec5aa09a904
robbiemartinus <robbie.martinus@yahoo.com.au>
2016-05-04 13:54:05 +1000

    java 7 feature

---
+++ src/main/java/com/googlecode/practicemyjava/java7/FileWatcher.java
@@ -0,0 +1,50 @@
+package com.googlecode.practicemyjava.java7;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+public class FileWatcher {
+    private static final Logger logger = LoggerFactory.getLogger(FileWatcher.class);
+
+    public static void main(String[] args) {
+        final Path path = Paths.get("./src/main/resources");
+
+        try {
+            while (true) {
+                logger.info("Watching folder {}", path.getFileName());
+                WatchService watcher = path.getFileSystem().newWatchService();
+                path.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+
+                logger.info("watching folder");
+                WatchKey watckKey = watcher.take();
+
+                List<WatchEvent<?>> events = watckKey.pollEvents();
+                for (WatchEvent event : events) {
+                    if (event.kind() == ENTRY_CREATE) {
+                        logger.info("Create event: {}", event.context().toString());
+                    }
+                    if (event.kind() == ENTRY_DELETE) {
+                        logger.info("Delete event: {}", event.context().toString());
+                    }
+                    if (event.kind() == ENTRY_MODIFY) {
+                        logger.info("Modify event: {}", event.context().toString());
+                    }
+                }
+            }
+        } catch (IOException | InterruptedException ex) {
+            logger.error("Error: {} " + ex.toString(), ex);
+        }
+    }
+}
