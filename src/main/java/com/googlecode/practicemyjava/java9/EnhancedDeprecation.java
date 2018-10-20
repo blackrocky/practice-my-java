d8f9a39c3688e17e9136a8f993bb3ec22b5398c8
Robbie Martinus <12573669+rmartinus@users.noreply.github.com>
2018-10-20 11:12:45 +1100

    Add Java 9 enhanced deprecation

---
+++ src/main/java/com/googlecode/practicemyjava/java9/EnhancedDeprecation.java
@@ -0,0 +1,8 @@
+package com.googlecode.practicemyjava.java9;
+
+public class EnhancedDeprecation {
+
+    @Deprecated(since = "1.0", forRemoval = true)
+    public void deprecatedMethod() {
+    }
+}
